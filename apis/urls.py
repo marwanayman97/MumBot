b277bb2e6c9744e0e83715f4af57ea10f174452f
Marwan Ayman <marwan.ayman97@gmail.com>
2021-06-01 19:05:13 +0200

    CRUD DRF API

--- apis/urls.py
+++ apis/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("api/parent/<int:id>/", views.api_parent_view, name="parentdetail"),
+    path("api/parent/create/", views.api_create_parent_view, name="parentcreate"),
+    path("api/parent/update/<int:id>/", views.api_update_parent_view, name="parentupdate"),
+    path("api/parent/delete/<int:id>/", views.api_delete_parent_view, name="parentdelete"),
     path("api/specialist/<int:id>/", views.api_specialist_view, name="specialistdetail"),
     path("api/inquiry/<int:id>/", views.api_inquiry_view, name="inquirydetail"),
     path("api/videosession/<int:id>/", views.api_video_view, name="videodetail"),
--- apis/views.py
+++ apis/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 from grad import models
 from .serializers import *
 
@@ -14,6 +15,42 @@ def api_parent_view(request, id):
     parent = models.Parent.objects.get(id=id)
     serializer = ParentSerializer(parent)
     return Response(serializer.data)
+
+@api_view(['POST', ])
+@csrf_exempt
+def api_create_parent_view(request):
+    
+    parent = models.Parent()
+    serializer = ParentSerializer(parent, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', ])
+@csrf_exempt
+def api_update_parent_view(request, id):
+    
+    parent = models.Parent.objects.get(id=id)
+    serializer = ParentSerializer(parent, data=request.data)
+    data = {}
+    if serializer.is_valid():
+        serializer.save()
+        data["success"] = "Update Successful"
+        return Response(data=data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE', ])
+def api_delete_parent_view(request, id):
+    
+    parent = models.Parent.objects.get(id=id)
+    operation = parent.delete()
+    data = {}
+    if operation:
+        data["success"] = "Delete Successful"
+    else:
+        data["failure"] = "Delete Failed"
+    return Response(data=data)
 
 @api_view(['GET', ])
 def api_specialist_view(request, id):
